85cc9ee0bc934909bdcc3bf3cc02ad2c93b3bfd4
Natalia Venditto <nataliafrontend@gmail.com>
2024-04-08 12:04:52 +0200

    feat: add conditional for type of build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,29 @@
 import { resolve } from 'node:path';
 // eslint-disable-next-line n/no-unpublished-import
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';  
 
-export default defineConfig({
-  build: {
-    emptyOutDir: true,
-    lib: {
-      // eslint-disable-next-line unicorn/prefer-module, no-undef
-      entry: resolve(__dirname, 'src/index.ts'),
-      name: 'chat-component',
-      fileName: 'chat-component',
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  const isLib = process.env.VITE_IS_LIB === 'true';
+
+  const config = {
+    entry: resolve(__dirname, 'src/index.ts'),
+    name: 'chat-component',
+    fileName: 'chat-component'
+  };
+
+  const distConfig = isLib ? config : undefined;
+  return defineConfig({
+    build: {
+      emptyOutDir: true,
+      lib: distConfig,
     },
-  },
-});
+    server: {
+      proxy: {
+        '/ask': 'http://127.0.0.1:3000',
+        '/chat': 'http://127.0.0.1:3000',
+        '/content': 'http://127.0.0.1:3000',
+      },
+    },
+  });
+}
